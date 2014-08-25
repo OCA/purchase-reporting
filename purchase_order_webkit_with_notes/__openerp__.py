802c89951bafe057bb6cea3a65f16193c412a34f
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2014-08-25 10:47:02 +0200

    wrap lines in addons description

--- purchase_order_webkit_with_notes/__openerp__.py
+++ purchase_order_webkit_with_notes/__openerp__.py
@@ -33,9 +33,11 @@
     'category': 'Reports/Webkit',
     'description': """
 Replaces the legacy rml Quotation / Purchase Order report by
-a brand new webkit report with header and footer notes on the purchase order and quotation
+a brand new webkit report with header and footer notes on the
+purchase order and quotation
 
-It's the same report as purchase_order_webkit, but add notes for header and footer
+It's the same report as purchase_order_webkit, but add notes
+for header and footer
     """,
     'author': 'Camptocamp',
     'website': 'http://www.camptocamp.com',
