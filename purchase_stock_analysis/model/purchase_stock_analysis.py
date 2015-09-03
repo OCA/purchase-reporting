843482318bb1d7540f047f9f67edbefe3c458af7
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-09-03 15:27:23 +0200

    split sql view in pieces
    This might allow to adapt the query in other modules.

--- purchase_stock_analysis/model/purchase_stock_analysis.py
+++ purchase_stock_analysis/model/purchase_stock_analysis.py
@@ -82,100 +82,124 @@ class PurchaseStockAnalysis(models.Model):
         tools.sql.drop_view_if_exists(cr, 'purchase_stock_analysis')
         cr.execute("""
             create or replace view purchase_stock_analysis as (
-                WITH currency_rate (currency_id, rate, date_start, date_end)
-                AS (
-                    SELECT r.currency_id, r.rate, r.name AS date_start,
-                        (SELECT name FROM res_currency_rate r2
-                        WHERE r2.name > r.name AND
-                            r2.currency_id = r.currency_id
-                         ORDER BY r2.name ASC
-                         LIMIT 1) AS date_end
-                    FROM res_currency_rate r
-                )
-                select
-                    min(l.id) as id,
-                    s.date_order as date_order,
-                    l.state,
-                    s.date_approve,
-                    s.minimum_planned_date as expected_date,
-                    s.dest_address_id,
-                    s.pricelist_id,
-                    s.validator,
-                    spt.warehouse_id as picking_type_id,
-                    s.partner_id as partner_id,
-                    s.create_uid as user_id,
-                    s.company_id as company_id,
-                    l.product_id,
-                    t.categ_id as category_id,
-                    t.uom_id as product_uom,
-                    s.location_id as location_id,
-                    sum(l.product_qty/u.factor*u2.factor) as unit_quantity,
-                    extract(epoch from age(s.date_approve,s.date_order)) /
-                        (24*60*60)::decimal(16,2) as delay,
+                {with_as}
+                {select}
+                {from_join}
+                {group_by}
+            );
+        """.format(with_as=self._with_as(),
+                   select=self._select(),
+                   from_join=self._from_join(),
+                   group_by=self._group_by(),
+                   ))
 
-                    extract(epoch from age(l.date_planned,s.date_order)) /
-                    (24*60*60)::decimal(16,2) as days_to_deliver,
+    def _with_as(self):
+        return """
+            WITH currency_rate (currency_id, rate, date_start, date_end)
+            AS (
+                SELECT r.currency_id, r.rate, r.name AS date_start,
+                    (SELECT name FROM res_currency_rate r2
+                    WHERE r2.name > r.name AND
+                        r2.currency_id = r.currency_id
+                        ORDER BY r2.name ASC
+                        LIMIT 1) AS date_end
+                FROM res_currency_rate r
+            )
+            """
 
-                    extract(epoch from move.date_expected - l.date_planned)
-                         / (24*60*60)::decimal(16,2)
-                         as days_initial_to_updated_schedule,
-                    extract(epoch from move.date - move.date_expected)
-                         / (24*60*60)::decimal(16,2)
-                         as days_schedule_to_actual_delivery,
+    def _select(self):
+        return """
+            SELECT
+                MIN(l.id) as id,
+                s.date_order as date_order,
+                l.state,
+                s.date_approve,
+                s.minimum_planned_date as expected_date,
+                s.dest_address_id,
+                s.pricelist_id,
+                s.validator,
+                spt.warehouse_id as picking_type_id,
+                s.partner_id as partner_id,
+                s.create_uid as user_id,
+                s.company_id as company_id,
+                l.product_id,
+                t.categ_id as category_id,
+                t.uom_id as product_uom,
+                s.location_id as location_id,
+                sum(l.product_qty/u.factor*u2.factor) as unit_quantity,
+                extract(epoch from age(s.date_approve,s.date_order)) /
+                    (24*60*60)::decimal(16,2) as delay,
 
-                    sum(l.price_unit * cr.rate * l.product_qty)::decimal(16, 2)
-                        as price_total,
-                    avg(100.0 * (l.price_unit * cr.rate * l.product_qty) /
-                        NULLIF(ip.value_float * l.product_qty / u.factor
-                            * u2.factor, 0.0))::decimal(16,2) as negociation,
-                    sum(ip.value_float * l.product_qty/u.factor
-                        * u2.factor)::decimal(16,2) as price_standard,
-                    (sum(l.product_qty*cr.rate*l.price_unit)/
-                        NULLIF(sum(l.product_qty/u.factor*u2.factor),
-                               0.0))::decimal(16,2) as price_average
-                from purchase_order_line l
-                    join purchase_order s on (l.order_id=s.id)
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t
-                                on (p.product_tmpl_id=t.id)
+                extract(epoch from age(l.date_planned,s.date_order)) /
+                (24*60*60)::decimal(16,2) as days_to_deliver,
+
+                extract(epoch from move.date_expected - l.date_planned)
+                        / (24*60*60)::decimal(16,2)
+                        as days_initial_to_updated_schedule,
+                extract(epoch from move.date - move.date_expected)
+                        / (24*60*60)::decimal(16,2)
+                        as days_schedule_to_actual_delivery,
+
+                SUM(l.price_unit * cr.rate * l.product_qty)::decimal(16, 2)
+                    as price_total,
+                AVG(100.0 * (l.price_unit * cr.rate * l.product_qty) /
+                    NULLIF(ip.value_float * l.product_qty / u.factor
+                        * u2.factor, 0.0))::decimal(16,2) as negociation,
+                SUM(ip.value_float * l.product_qty/u.factor
+                    * u2.factor)::decimal(16,2) as price_standard,
+                (SUM(l.product_qty*cr.rate*l.price_unit)/
+                    NULLIF(sum(l.product_qty/u.factor*u2.factor),
+                            0.0))::decimal(16,2) as price_average
+        """
+
+    def _from_join(self):
+        return """
+                FROM purchase_order_line l
+                    JOIN purchase_order s on (l.order_id=s.id)
+                        LEFT JOIN product_product p on (l.product_id=p.id)
+                            LEFT JOIN product_template t
+                                ON (p.product_tmpl_id=t.id)
                             LEFT JOIN ir_property ip ON
                                 (ip.name='standard_price'
                                  AND ip.res_id=CONCAT('product.template,',t.id)
                                  AND ip.company_id=s.company_id)
-                    left join product_uom u on (u.id=l.product_uom)
-                    left join product_uom u2 on (u2.id=t.uom_id)
-                    left join stock_picking_type spt
+                    LEFT JOIN product_uom u on (u.id=l.product_uom)
+                    LEFT JOIN product_uom u2 on (u2.id=t.uom_id)
+                    LEFT JOIN stock_picking_type spt
                         on (spt.id=s.picking_type_id)
-                    join currency_rate cr on (cr.currency_id = s.currency_id
-                        and cr.date_start <= coalesce(s.date_order, now()) and
-                        (cr.date_end is null
-                         or cr.date_end > coalesce(s.date_order, now())))
-                    left join stock_move move on (move.purchase_line_id=l.id)
-                group by
-                    s.company_id,
-                    s.create_uid,
-                    s.partner_id,
-                    u.factor,
-                    s.location_id,
-                    l.price_unit,
-                    s.date_approve,
-                    l.date_planned,
-                    l.product_uom,
-                    s.minimum_planned_date,
-                    s.pricelist_id,
-                    s.validator,
-                    s.dest_address_id,
-                    l.product_id,
-                    t.categ_id,
-                    s.date_order,
-                    l.state,
-                    spt.warehouse_id,
-                    u.uom_type,
-                    u.category_id,
-                    t.uom_id,
-                    u.id,
-                    u2.factor,
-                    move.date,
-                    move.date_expected
-            )
-        """)
+                    JOIN currency_rate cr ON (cr.currency_id = s.currency_id
+                        AND cr.date_start <= coalesce(s.date_order, now()) AND
+                        (cr.date_end IS NULL
+                         OR cr.date_end > COALESCE(s.date_order, now())))
+                    LEFT JOIN stock_move move ON (move.purchase_line_id=l.id)
+                    """
+
+    def _group_by(self):
+        return """
+            GROUP BY
+            s.company_id,
+            s.create_uid,
+            s.partner_id,
+            u.factor,
+            s.location_id,
+            l.price_unit,
+            s.date_approve,
+            l.date_planned,
+            l.product_uom,
+            s.minimum_planned_date,
+            s.pricelist_id,
+            s.validator,
+            s.dest_address_id,
+            l.product_id,
+            t.categ_id,
+            s.date_order,
+            l.state,
+            spt.warehouse_id,
+            u.uom_type,
+            u.category_id,
+            t.uom_id,
+            u.id,
+            u2.factor,
+            move.date,
+            move.date_expected
+            """
