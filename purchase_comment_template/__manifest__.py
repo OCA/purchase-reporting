da4379461ab586ef0664ea7116feb0ec211d0af7
mreficent <miquel.raich@eficent.com>
2018-07-20 15:14:08 +0200

    [ADD] purchase_comment_template

---
+++ purchase_comment_template/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{"name": "Purchase Comments",
+ "summary": "Comments texts templates on Purchase documents",
+ "version": "10.0.1.0.0",
+ "category": "Purchase",
+ "website": "https://github.com/OCA/purchase-reporting",
+ "author": "Eficent, Odoo Community Association (OCA)",
+ "license": "AGPL-3",
+ "depends": ["purchase",
+             "invoice_comment_template",
+             ],
+ "data": ['security/ir.model.access.csv',
+          'views/purchase_order_view.xml',
+          'views/base_comment_template_view.xml',
+          'views/report_purchaseorder.xml',
+          'views/report_quotation.xml',
+          ],
+ 'installable': True,
+ }
