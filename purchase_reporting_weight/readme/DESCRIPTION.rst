b4c1d45f048da8a4899b15cbcbf4abeb038cbf90
David <david.vidal@tecnativa.com>
2018-10-29 11:59:56 +0100

    [MIG] purchase_reporting_weight: Migration to 11.0

---
+++ purchase_reporting_weight/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module adds another possible source to the purchase analysis weight value:
+
+* If the UoM of the product is one of the category "Weight", the value is taken
+  from the ordered quantity.
--- purchase_reporting_weight/reports/purchase_report.py
+++ purchase_reporting_weight/reports/purchase_report.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2018 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models, tools
-import openerp.addons.decimal_precision as dp
+from odoo import api, models, tools
 
 
 class PurchaseReport(models.Model):
     _inherit = "purchase.report"
-
-    weight = fields.Float(digits=dp.get_precision('Stock Weight'))
 
     def _select(self):
         return """
@@ -30,16 +27,17 @@ class PurchaseReport(models.Model):
     def _group_by(self):
         return ", p.weight, u.category_id, imd.res_id"
 
-    def init(self, cr):
+    @api.model_cr
+    def init(self):
         """Inject parts in the query with this hack, fetching the query and
         recreating it. Query is returned all in upper case and with final ';'.
         """
-        super(PurchaseReport, self).init(cr)
-        cr.execute("SELECT pg_get_viewdef(%s, true)", (self._table,))
-        view_def = cr.fetchone()[0]
+        super(PurchaseReport, self).init()
+        self._cr.execute("SELECT pg_get_viewdef(%s, true)", (self._table,))
+        view_def = self._cr.fetchone()[0]
         view_def = view_def.replace(
-            "FROM purchase_order_line",
-            "{} FROM purchase_order_line".format(self._select()),
+            "sum(p.weight * l.product_qty/u.factor*u2.factor) as weight,",
+            "{}".format(self._select()),
         )
         view_def = view_def.replace(
             "GROUP BY", " {} GROUP BY".format(self._from()),
@@ -48,7 +46,7 @@ class PurchaseReport(models.Model):
             view_def = view_def[:-1]
         view_def += self._group_by()
         # Re-create view
-        tools.drop_view_if_exists(cr, self._table)
-        cr.execute("create or replace view {} as ({})".format(
+        tools.drop_view_if_exists(self._cr, self._table)
+        self._cr.execute("create or replace view {} as ({})".format(
             self._table, view_def,
         ))
