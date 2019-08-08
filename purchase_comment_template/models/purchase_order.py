04b538085b95c3db7fcb6e8f1244e9acfd3a8f6c
mreficent <miquel.raich@eficent.com>
2019-08-08 20:37:22 +0200

    [MIG] purchase_comment_template: Migration to 12.0

--- purchase_comment_template/models/purchase_order.py
+++ purchase_comment_template/models/purchase_order.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -28,6 +28,15 @@ class PurchaseOrder(models.Model):
         comment = self.comment_template2_id
         if comment:
             self.note2 = comment.get_value(self.partner_id.id)
+
+    @api.onchange('partner_id')
+    def onchange_partner_id_purchase_comment(self):
+        if self.partner_id:
+            comment_template = self.partner_id.property_comment_template_id
+            if comment_template.position == 'before_lines':
+                self.comment_template1_id = comment_template
+            elif comment_template.position == 'after_lines':
+                self.comment_template2_id = comment_template
 
 
 class PurchaseOrderLine(models.Model):
--- purchase_comment_template/tests/test_purchase_order_report.py
+++ purchase_comment_template/tests/test_purchase_order_report.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
-from odoo import report
 
 
 class TestPurchaseOrderReport(TransactionCase):
-    def setUp(self, *args, **kwargs):
+    def setUp(self):
         super(TestPurchaseOrderReport, self).setUp()
         self.base_comment_model = self.env['base.comment.template']
         self.before_comment = self._create_comment('before_lines')
         self.after_comment = self._create_comment('after_lines')
-
+        self.partner_id = self.env['res.partner'].create({
+            'name': 'Partner Test'
+        })
         self.purchase_order = self.env.ref('purchase.purchase_order_4')
         self.purchase_order.update({
             'comment_template1_id': self.before_comment.id,
@@ -29,8 +29,26 @@ class TestPurchaseOrderReport(TransactionCase):
         })
 
     def test_comments_in_purchase_order(self):
-        (res, _) = report. \
-            render_report(self.env.cr, self.env.uid, [self.purchase_order.id],
-                          'purchase.report_purchaseorder', {})
-        self.assertRegexpMatches(res, self.before_comment.text)
-        self.assertRegexpMatches(res, self.after_comment.text)
+        res = self.env['ir.actions.report']._get_report_from_name(
+            'purchase.report_purchaseorder'
+        ).render_qweb_html(self.purchase_order.ids)
+        self.assertRegexpMatches(str(res[0]), self.before_comment.text)
+        self.assertRegexpMatches(str(res[0]), self.after_comment.text)
+
+    def test_onchange_partner_id(self):
+        self.partner_id.property_comment_template_id = self.after_comment.id
+        vals = {
+            'partner_id': self.partner_id.id,
+        }
+        new_purchase = self.env['purchase.order'].new(vals)
+        new_purchase.onchange_partner_id_purchase_comment()
+        purchase_dict = new_purchase._convert_to_write(new_purchase._cache)
+        new_purchase = self.env['purchase.order'].create(purchase_dict)
+        self.assertEqual(new_purchase.comment_template2_id, self.after_comment)
+        self.partner_id.property_comment_template_id = self.before_comment.id
+        new_purchase = self.env['purchase.order'].new(vals)
+        new_purchase.onchange_partner_id_purchase_comment()
+        purchase_dict = new_purchase._convert_to_write(new_purchase._cache)
+        new_purchase = self.env['purchase.order'].create(purchase_dict)
+        self.assertEqual(
+            new_purchase.comment_template1_id, self.before_comment)
