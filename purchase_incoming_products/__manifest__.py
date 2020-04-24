51c7de076690b55b7800663870f94fe9901a3dcd
tino480 <2tino480@gmail.com>
2020-04-24 14:23:45 -0500

    [13.0][ADD] purchase_incoming_products

---
+++ purchase_incoming_products/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Incoming Products In Purchase",
+    "summary": "Module that generates a view for incoming products",
+    "version": "13.0.1.0.0",
+    "category": "Reporting",
+    "author": "Jarsa Sistemas,Odoo SA,Odoo Community Association (OCA)",
+    "website": "https://www.jarsa.com.mx",
+    "license": "LGPL-3",
+    "depends": ["purchase", "stock"],
+    "data": ["views/purchase_incoming_products.xml"],
+}
---
+++ purchase_incoming_products/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Go to purchase/ reports /incoming products press on the button
+and the report will be generated.
