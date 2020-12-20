48ce6d0c7aa8d47bf52ed431b75637ee9c749574
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-12-20 09:13:15 -0600

    [MIG] purchase_incoming_products: Migration to 14.0

--- purchase_incoming_product/__manifest__.py
+++ purchase_incoming_product/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Incoming Products In Purchase",
     "summary": "Module that generates a view for incoming products",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Reporting",
     "author": "Jarsa Sistemas,Odoo SA,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-reporting",
--- purchase_incoming_product/readme/CONTRIBUTORS.rst
+++ purchase_incoming_product/readme/CONTRIBUTORS.rst
@@ -1,3 +1,4 @@
 * `Jarsa Sistemas, S.A. de C.V. <http://www.jarsa.com.mx>`_:
 
   * Juventino Villegas <juventino.villegas@jarsa.com.mx>
+  * Alan Ramos <alan.ramos@jarsa.com.mx>
