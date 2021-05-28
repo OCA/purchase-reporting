219724854c377dabe17333c51fe857b6dcf4c42b
Chanakya Soni <csoni@opensourceintegrators.com>
2021-05-28 16:54:16 +0530

    [14.0][MIG] purchase_backorder.
    [MIG] Pre-Commit
    
    [MIG] Prettier
    
    [MIG] Prettier

--- purchase_backorder/README.rst
+++ purchase_backorder/README.rst
@@ -73,6 +73,7 @@ Contributors
 * Michael Allen <mallen@opensourceintegrators.com>
 * Sandip Mangukiya <smangukiya@opensourceintegrators.com>
 * Bhavesh Odedra <bodedra@opensourceintegrators.com>
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
 
 Maintainers
 ~~~~~~~~~~~
--- purchase_backorder/models/purchase.py
+++ purchase_backorder/models/purchase.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2012 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
+# Copyright (C) 2021 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -21,7 +22,6 @@ class PurchaseOrder(models.Model):
         string="Backorder Value", compute="_compute_bo_value", store=True
     )
 
-    @api.multi
     @api.depends("order_line.uigr_qty", "order_line.price_unit")
     def _compute_uigr_value(self):
         for order in self:
@@ -30,7 +30,6 @@ class PurchaseOrder(models.Model):
                 total += line.uigr_value
             order.uigr_value = total
 
-    @api.multi
     @api.depends("order_line.bo_qty", "order_line.price_unit")
     def _compute_bo_value(self):
         for order in self:
@@ -39,7 +38,6 @@ class PurchaseOrder(models.Model):
                 total += line.bo_value
             order.bo_value = total
 
-    @api.multi
     @api.depends("order_line.last_date_received")
     def _compute_last_date_received(self):
         for order in self:
@@ -52,6 +50,7 @@ class PurchaseOrder(models.Model):
                     max_date = line.last_date_received
             order.last_date_received = max_date
 
+    @api.depends("order_line.last_bill_date")
     def _compute_last_bill_date(self):
         for order in self:
             max_date = False
@@ -85,31 +84,26 @@ class PurchaseOrderLine(models.Model):
         related="product_id.product_tmpl_id.type", string="Product Type"
     )
 
-    @api.multi
     @api.depends("qty_received", "product_qty")
     def _compute_bo_qty(self):
         for line in self:
             line.bo_qty = line.product_qty - line.qty_received
 
-    @api.multi
     @api.depends("qty_received", "qty_invoiced")
     def _compute_uigr_qty(self):
         for line in self:
             line.uigr_qty = line.qty_received - line.qty_invoiced
 
-    @api.multi
     @api.depends("uigr_qty", "price_unit")
     def _compute_uigr_value(self):
         for line in self:
             line.uigr_value = line.uigr_qty * line.price_unit
 
-    @api.multi
     @api.depends("bo_qty", "price_unit")
     def _compute_bo_value(self):
         for line in self:
             line.bo_value = line.bo_qty * line.price_unit
 
-    @api.multi
     @api.depends("order_id.state", "move_ids.state", "move_ids.product_uom_qty")
     def _compute_last_date_received(self):
         for line in self:
@@ -127,19 +121,19 @@ class PurchaseOrderLine(models.Model):
                             max_date = move.date
             line.last_date_received = max_date
 
-    @api.depends("invoice_lines.invoice_id.state", "invoice_lines.quantity")
+    @api.depends("invoice_lines.move_id.state", "invoice_lines.quantity")
     def _compute_last_bill_date(self):
         for line in self:
             max_date = False
             for inv_line in line.invoice_lines:
-                if inv_line.invoice_id.state not in ["cancel"]:
-                    if inv_line.invoice_id.type == "in_invoice":
-                        if max_date and inv_line.invoice_id.date:
-                            if max_date < inv_line.invoice_id.date:
-                                max_date = inv_line.invoice_id.date
+                if inv_line.move_id.state not in ["cancel"]:
+                    if inv_line.move_id.move_type == "in_invoice":
+                        if max_date and inv_line.move_id.date:
+                            if max_date < inv_line.move_id.date:
+                                max_date = inv_line.move_id.date
                         else:
-                            max_date = inv_line.invoice_id.date
-                    elif inv_line.invoice_id.type == "in_refund":
+                            max_date = inv_line.move_id.date
+                    elif inv_line.move_id.move_type == "in_refund":
                         continue
             line.last_bill_date = (
                 max_date and max_date.strftime(DEFAULT_SERVER_DATETIME_FORMAT) or False
