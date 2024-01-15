aaf42e7e4fdf7c0a6c6ebefafb137cda7fa9518d
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-01-15 21:00:25 +0100

    Add module Purchase Requisition Blanket Order Report

---
+++ purchase_requisition_blanket_order_report/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Purchase Requisition Blanket Order Report",
+    "summary": "Improvement of call for tenders report for blanket orders",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/purchase-reporting",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["grindtildeath"],
+    "license": "AGPL-3",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        "report/report_purchase_requisition.xml",
+    ],
+}
