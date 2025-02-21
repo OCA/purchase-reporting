089063c5d0613bc581f43ee5e2f689222a57105c
Bhavesh Heliconia <oca@heliconia.in>
2025-02-21 12:21:45 +0530

    [MIG] purchase_packaging_report: Migration to 17.0

--- purchase_packaging_report/__manifest__.py
+++ purchase_packaging_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Purchase Packaging Report",
     "summary": "Packaging data in purchase reports",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Inventory/Purchase",
     "website": "https://github.com/OCA/purchase-reporting",
