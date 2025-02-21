6c30e81e62589f8f60df15de5afe89e6f8b98452
Bhavesh Heliconia <oca@heliconia.in>
2025-02-21 12:29:24 +0530

    [MIG] purchase_report_payment_term: Migration to 17.0

--- purchase_report_payment_term/README.rst
+++ purchase_report_payment_term/README.rst
@@ -54,6 +54,13 @@ Authors
 
 * Quartile Limited
 
+Contributors
+------------
+
+- `Heliconia Solutions Pvt. Ltd. <https://www.heliconia.io>`__
+
+  - Bhavesh Heliconia
+
 Maintainers
 -----------
 
--- purchase_report_payment_term/__manifest__.py
+++ purchase_report_payment_term/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Report Payment Term",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Purchase",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-reporting",
