02b8e6bf6511adfa0f03f709fb60a3ab1eb77a7c
David Vidal <david@moduon.team>
2025-10-30 15:56:57 +0100

    [ADD] purchase_order_report_hide_tax: New module
    MT-11623

---
+++ purchase_order_report_hide_tax/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+{
+    "name": "Purchase order line hide tax in report",
+    "summary": "Hide taxes column when they don't add value",
+    "version": "16.0.1.0.2",
+    "development_status": "Alpha",
+    "category": "Purchases",
+    "website": "https://github.com/OCA/purchase-reporting",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu", "rafaelbn"],
+    "license": "LGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "reports/purchase_order_report_template.xml",
+    ],
+}
---
+++ purchase_order_report_hide_tax/readme/CONTEXT.md
@@ -0,0 +1,3 @@
+When the taxes are the same in every purchase line, that information might be redundant with the taxes summary and takes valuable space that other columns might take.
+
+This might be event more annoying when there's tax exemptions like in EU intracomunitary operations.
