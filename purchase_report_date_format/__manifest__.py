08088538b2e53786ddabc9c09314383a68a68bb5
Bhavesh Heliconia <oca@heliconia.in>
2025-12-26 12:52:43 +0530

    [MIG] purchase_report_date_format: Migration to 19.0

--- purchase_report_date_format/__manifest__.py
+++ purchase_report_date_format/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Report Date Format",
     "category": "Reporting",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-reporting",
     "license": "AGPL-3",
