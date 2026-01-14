6470472900f7ebc75563486bef1d68642e55aa1e
Aungkokolin1997 <lin@quartile.co>
2026-01-14 10:49:30 +0000

    [ADD] purchase_report_discount_visibility

---
+++ purchase_report_discount_visibility/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Report Discount Visibility",
+    "summary": "Configure visibility of discount column in purchase reports",
+    "version": "18.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/purchase-reporting",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "depends": ["purchase"],
+    "data": [
+        "report/purchase_report_templates.xml",
+    ],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
+    "installable": True,
+}
